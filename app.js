e1bac496e1dc23053e921dac90a5f38208c25c60
swordmaster2005 <soulmaster9794@gmail.com>
2023-07-18 02:18:06 +0530

    UPGRADE2.0

--- app.js
+++ app.js
@@ -6,47 +6,49 @@ let score=document.querySelector(".score")
 let gameOver=document.querySelector(".time")
 let modal=document.querySelector(".gameOver")
 let short_info=document.querySelector(".short__info")
-let next=document.querySelector(".next")
+let form=document.querySelector(".user__info")
 let fon=document.querySelector("body")
-let starBox=document.querySelector(".game__start")
-let ism=document.querySelector(".ism")
-let familya=document.querySelector(".familya")
+let register=document.querySelector(".register")
+let userName=document.querySelector(".ism")
 let level=document.querySelector(".choose__level")
 let user=document.querySelector(".user")
+let start_game=document.querySelector(".start-game")
 
+let email=document.querySelector(".email")
+let password=document.querySelector(".password")
 
+// chose level
 let easybtn=document.querySelector(".easy")
 let mediumbtn=document.querySelector(".medium")
 let hardbtn=document.querySelector(".hard")
+// 
+// audio
+let saund=document.querySelector("#sau")
+// 
 
+
+const users=JSON.parse(localStorage.getItem('users'))? JSON.parse(localStorage.getItem('users')):[]
 
 let rep =document.querySelector(".replay").addEventListener("click" ,()=>{
-    ball=0
-    time=11
+    trt.textContent="5"
     allWorks()
-    modal.style.display = "none";
+    closeWindow(modal)
+    closeWindow(box)
+    openWindow(level)
+    begin=6
+    fon.className="level__fon"
 
-    
-    const timeinterval= setInterval(function () {
-        if(time>0){
-            time--
-            gameOver.textContent=`Time:${time}s`
-        }
-        else{
-          
-            openModal()
-            clearInterval(timeinterval)
-            
-        }
-    }, 1000);
 })
 
-
+// foyadalanuchi malumotlari
+let nickName
+let passwordLoc
+let emailLoc
 
 
 let ball=0
 let time=10
-let begin=0
+let begin=5
 
 
 // level
@@ -78,72 +80,118 @@ function allWorks(level) {
 }
 
 
-  
-    function openModal() {
-        modal.style.display = "flex";
-    }
-
-
-
-function bb(){
-    modal.style.display = "none";
-}
-next.addEventListener("click",()=>{
-    if(ism.value&&familya.value){
-        starBox.style.display="none"
-        level.classList.remove("none")
-        fon.className="level__fon"
-    }
-   
-else{
-    alert("Malumotlaringiz to'liq emas")
-}
+form.addEventListener("submit",(e)=>{
+    e.preventDefault()
+    nickName=userName.value
+    passwordLoc=password.value
+    emailLoc=email.value
+    closeWindow(register)
+    openWindow(level)
+    fon.className="level__fon"
 })
+let trt=document.querySelector(".trt")
 
 const game=()=>{
     const begintime=setInterval(()=>{
         if(begin>0){
             begin--
+            trt.textContent=`${begin}`
+            bg_change(begin)
         }
         else{
-            box.style.display="flex"
-            
+            ball=0
+            time=10
+            openWindow(box)
+            input.value=null
+            saund.pause()
             input.focus()
-            fon.classList.remove("start__fon")
-            fon.style.backgroundImage = "url('./images/play__time.jpg')";
+            fon.className="play-fon"
             console.log("Salom");
-            const timeinterval= setInterval(function () {
-                if(time>0){
-                    time--
-                    gameOver.textContent=`Time:${time}s`
-                }
-                else{
-                  
-                    openModal()
-                    
-                    user.textContent=`${familya.value} ${ism.value} `
-                    short_info.textContent=`Your score ${ball}`
-                    clearInterval(timeinterval)
-                    
-                }
-            }, 1000);
+            limitTime()
+            closeWindow(start_game)
             clearInterval(begintime)
         }
-           },4000)
+           },1000)
 }
 
 easybtn.addEventListener("click",()=>{
-    level.style.display="none"
+    closeWindow(level)
+    openWindow(start_game)
     game()
     allWorks(easy)
 })
 mediumbtn.addEventListener("click",()=>{
-    level.style.display="none"
+    closeWindow(level)
+    openWindow(start_game)
     game()
     allWorks(medium)
 })
 hardbtn.addEventListener("click",()=>{
-    level.style.display="none"
+    closeWindow(level)
+    openWindow(start_game)
     game()
     allWorks(hard)
 })
+
+
+
+// open && close
+
+function openWindow(open){
+return open.style.display="flex"
+}
+function closeWindow(close){
+    return close.style.display="none"
+}
+// open && close end
+
+// localstorage infoUser
+
+
+
+function limitTime(){
+    const timeinterval= setInterval(function () {
+          if(time>0){
+              time--
+              gameOver.textContent=`Time:${time}s`
+          }
+          else{
+              openWindow(modal)
+              user.textContent=userName.value
+              short_info.textContent=`Your score ${ball}`
+              console.log("salom");
+              clearInterval(timeinterval)
+              let  userMainInfo={
+                nickName,
+                passwordLoc,
+                ball}
+            users.push(userMainInfo)
+            localStorage.setItem("users", JSON.stringify(users))
+          }
+      }, 1000);
+  }
+let hi= localStorage.getItem("users",JSON.parse(users))
+console.log(hi);
+  
+function bg_change(n){
+    if(n==4){
+        saund.play()
+        start_game.classList.add("red")
+       
+    }
+    else if(n==3){
+        start_game.classList.remove("red")
+    } 
+    else if(n==2){
+        start_game.classList.add("red")
+       
+    }
+    else if(n==1){
+        start_game.classList.remove("red")
+    } 
+    else if(n==0){
+        start_game.classList.add("red")
+    }
+}
+
+
